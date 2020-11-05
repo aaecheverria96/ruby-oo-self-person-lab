49158095c43c5cd96081a6174934632075746bdb
Your Username <you@example.com>
2020-11-05 15:14:29 -0500

    commit tree 8734fbf0badcdcafeeae8c0f7c8b5a4f82c74734

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,17 @@
 # your code goes here
+
+class Person 
+
+attr_accessor :bank_acount, :person 
+attr_reader :name 
+
+def initialize(name) 
+    @name = name 
+    @bank_account = 25
+
+end 
+
+
+
+
+end 
